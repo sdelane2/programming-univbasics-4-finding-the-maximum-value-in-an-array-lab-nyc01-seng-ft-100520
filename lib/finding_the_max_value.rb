08478207ee45f829ac3099c4a22e553b2aef9abc
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-17 18:00:50 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,5 +1,11 @@
 def find_max_value(array)
   counter = 0 
-  while array[counter] do 
-    
+  max_value = -1
+  while array[counter] do
+    if max_value < array[counter]
+      max_value = array[counter]
+    end
+    counter += 1
+  end
+  max_value
 end
